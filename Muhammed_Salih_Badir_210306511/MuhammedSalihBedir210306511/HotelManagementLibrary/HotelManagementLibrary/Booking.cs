8f83c135b8c19ab9a42bb986a256b4d0e9f7523e
MuhammedSalihBedir <saifbader700@gmail.com>
2024-12-24 13:22:49 +0300

    Add files via upload

---
+++ Muhammed_Salih_Badir_210306511/MuhammedSalihBedir210306511/HotelManagementLibrary/HotelManagementLibrary/Booking.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HotelManagementLibrary
+{
+    public class Booking
+    {
+        public IRoom Room { get; }
+        public Customer Customer { get; }
+        public DateTime BookingDate { get; }
+
+        public Booking(IRoom room, Customer customer)
+        {
+            Room = room;
+            Customer = customer;
+            BookingDate = DateTime.Now;
+        }
+
+        public override string ToString() =>
+            $"Booking: {Customer.Name} -> {Room.GetRoomType()} (Room {Room.RoomNumber}) on {BookingDate}";
+    }
+}
---
+++ Muhammed_Salih_Badir_210306511/MuhammedSalihBedir210306511/HotelManagementLibrary/HotelManagementLibrary/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagementLibrary
+{
+    public abstract class Person
+    {
+        public string Name { get; set; }
+        public string Contact { get; set; }
+
+        protected Person(string name, string contact)
+        {
+            Name = name;
+            Contact = contact;
+        }
+    }
+
+    public class Customer : Person
+    {
+        public Customer(string name, string contact) : base(name, contact) { }
+
+        public override string ToString() =>
+            $"Customer: {Name}, Contact: {Contact}";
+    }
+}
